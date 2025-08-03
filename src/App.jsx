c8784e7aad04b45a14ad7a25372535ec5603a38b
Alishak003 <dopex@gmail.com>
2025-08-03 19:20:29 +0530

    hmmmm

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+  import { BrowserRouter, Route, Routes } from 'react-router-dom';
+  import './App.css';
+  import {Login} from './pages/login';
+  import {Register} from './pages/register';
+  import { Comments } from './pages/Comments';
+  import Home from './pages/home';
+  import { Profile } from './pages/profile';
+  import { CompleteProfile } from './pages/profileComplete';
+  import GuestRoute from './guards/GuestOnly';
+  import LoggedinRoute from './guards/LoggedinROute';
 
-function App() {
-  const [count, setCount] = useState(0)
+  function App() {
+    return (
+      <BrowserRouter>
+      <Routes>
+        <Route path="/feed" element={<Home />} />
+        <Route path="/" element={<GuestRoute><Login /></GuestRoute>} />
+        <Route path="/register" element={<GuestRoute><Register /></GuestRoute>} />
+        <Route path="/completeProfile" element={<CompleteProfile />} />
+        <Route path="/profile" element={<LoggedinRoute><Profile/></LoggedinRoute>} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/comments/:postId" element={<Comments />} />
+      </Routes>
+      </BrowserRouter>
+    );
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+  export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from './contexts/authContext';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// Create the query client instance
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+          <QueryClientProvider client={queryClient}>
+                <App />
+    
+          </QueryClientProvider>
+
+    </AuthProvider>
   </StrictMode>,
 )
